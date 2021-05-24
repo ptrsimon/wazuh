d91b035e1eec08a7bd61bcb3d006f84f21da9013
Antonio Fresneda <antonio.fresneda@wazuh.com>
2021-05-24 16:18:15 +0200

    Update rt counter after deleting watch.

--- src/syscheckd/run_realtime.c
+++ src/syscheckd/run_realtime.c
@@ -392,6 +392,7 @@ void CALLBACK RTCallBack(DWORD dwerror, DWORD dwBytes, LPOVERLAPPED overlap)
     if(rtlocald->watch_status == FIM_RT_HANDLE_CLOSED) {
         w_mutex_lock(&syscheck.fim_realtime_mutex);
         rtlocald = OSHash_Delete_ex(syscheck.realtime->dirtb, wdchar);
+        syscheck.realtime->fd = OSHash_Get_Elem_ex(syscheck.realtime->dirtb);
         free_win32rtfim_data(rtlocald);
         mdebug1(FIM_REALTIME_CALLBACK, wdchar);
         w_mutex_unlock(&syscheck.fim_realtime_mutex);
@@ -459,7 +460,7 @@ int realtime_start() {
     }
     OSHash_SetFreeDataPointer(syscheck.realtime->dirtb, (void (*)(void *))free_win32rtfim_data);
 
-    syscheck.realtime->fd = -1;
+    syscheck.realtime->fd = 0;
     syscheck.realtime->evt = CreateEvent(NULL, TRUE, FALSE, NULL);
 
     return (0);
@@ -568,7 +569,6 @@ int realtime_adddir(const char *dir, directory_t *configuration, __attribute__((
             w_mutex_unlock(&syscheck.fim_realtime_mutex);
             return (0);
         }
-        syscheck.realtime->fd++;
 
         /* Add final elements to the hash */
         os_strdup(dir, rtlocald->dir);
@@ -587,6 +587,7 @@ int realtime_adddir(const char *dir, directory_t *configuration, __attribute__((
             merror_exit(FIM_CRITICAL_ERROR_OUT_MEM);
         }
 
+        syscheck.realtime->fd = OSHash_Get_Elem_ex(syscheck.realtime->dirtb);
         mdebug1(FIM_REALTIME_NEWDIRECTORY, dir);
     }
 
@@ -608,6 +609,7 @@ int fim_check_realtime_directory(win32rtfim *rtlocald) {
             mdebug1(FIM_REALTIME_CALLBACK, rtlocald->dir);
             rtlocald = OSHash_Delete_ex(syscheck.realtime->dirtb, rtlocald->dir);
             free_win32rtfim_data(rtlocald);
+            syscheck.realtime->fd = OSHash_Get_Elem_ex(syscheck.realtime->dirtb);
             return 1;
         }
 
